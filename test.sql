b956a1f33a9542cecb482e3267aca681c8defe58
JR Parker <github@rt250.com>
2020-09-11 13:43:24 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -4,4 +4,4 @@ SELECT TOP (1000) [Id]
       ,[SegmentName]
       ,[SegmentType]
       ,[Segment_Key]
-  FROM [ButlerTillETL].[source].[GASegments] WHERE Segment_Id = 'gaid::07N1aZPuQ2W-UjS5cmOX2A'
+  FROM [ButlerTillETL].[source].[GASegments]
